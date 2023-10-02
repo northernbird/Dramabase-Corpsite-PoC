10270cc4c297d55f63e1b2a00a5425f0869a4f0a
rmiyazaki-dramabase-inc <miyazaki@dramabase-inc.com>
2023-10-02 15:00:07 +0900

    feat(Form): サーバサイドへフォーム送信するよう修正 - フォームバリデーションの導入 - 送信完了後の文言表示エフェクト追加 - 入力データを受け取りログ表示させるサーバ側処理の実装

--- app/_components/form/RequestForm.js
+++ app/_components/form/RequestForm.js
@@ -1,84 +1,91 @@
 'use client'
 
-import 'flowbite'
 import Image from 'next/image'
-import React, {useEffect, useRef} from 'react'
+import React from 'react'
 import SendButton from '../../_assets/button/send.png'
+import {useForm} from "react-hook-form";
 
 /*
  * TODO: Consider to resolve code duplication with InquiryForm
  */
-export default function RequestForm({show}) {
-  const companyNameInput = useRef(null)
+export default function RequestForm({errorMessage, submittedMessage, onSubmitHandler}) {
+    const {
+        register,
+        handleSubmit,
+        formState: {errors}
+    } = useForm();
 
-  useEffect(() => {
-    if (show) {
-      // モーダル表示時に企業名欄にフォーカスする
-      companyNameInput.current.focus();
-    }
-  }, [show])
-
-  return (
-      <div className="px-3 py-4">
-        <h3 className="mb-2 text-base md:text-xl text-white text-center">資料請求</h3>
-         <form className="space-y-4" action={`mailto: ${process.env.NEXT_PUBLIC_INQUIRY_MAIL}?subject= ${process.env.NEXT_PUBLIC_REQUEST_MAIL_SUBJECT}`} method="POST">
-          <div>
-            <label
-                htmlFor="company_name"
-                className="block mb-1 text-xs text-white tracking-widest"
-            >
-              企業名
-            </label>
-            <input
-                type="text"
-                name="company_name"
-                id="company_name"
-                className="bg-gray-300 border border-gray-400 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-1"
-                placeholder=""
-                required
-                ref={companyNameInput}
-            />
-          </div>
-          <div>
-            <label
-                htmlFor="name"
-                className="block mb-1 text-xs text-white tracking-widest"
-            >
-              氏名
-            </label>
-            <input
-                type="text"
-                name="name"
-                id="name"
-                placeholder=""
-                className="bg-gray-300 border border-gray-400 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-1"
-                required
-            />
-          </div>
-          <div>
-            <label
-                htmlFor="email"
-                className="block mb-1 text-xs text-white tracking-widest"
-            >
-              Mail
-            </label>
-            <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder=""
-                className="bg-gray-300 border border-gray-400 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-1"
-                required
-            />
-          </div>
-          <button
-              type="submit"
-              className="w-full cursor-pointer hover:opacity-80 px-2 pt-3 pb-5"
-          >
-            <Image src={SendButton} alt="送信ボタン" className="object-contain"/>
-          </button>
-        </form>
-      </div>
-  )
+    return (
+        <div className="px-3 py-4">
+            <h3 className="mb-2 text-base md:text-xl text-white text-center">資料請求</h3>
+            {submittedMessage()}
+            <form className="space-y-4"
+                  onSubmit={handleSubmit((data, e) => {
+                      onSubmitHandler(data, e, "request")
+                  })}
+                  method="POST">
+                <div>
+                    <label
+                        htmlFor="company_name"
+                        className="block mb-1 text-xs text-white tracking-widest"
+                    >
+                        企業名
+                    </label>
+                    <input
+                        type="text"
+                        name="company_name"
+                        id="company_name"
+                        className="bg-gray-300 border border-gray-400 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-1"
+                        placeholder=""
+                        // required
+                        {...register("company_name", {required: true})}
+                    />
+                    {errors.company_name && errorMessage}
+                </div>
+                <div>
+                    <label
+                        htmlFor="name"
+                        className="block mb-1 text-xs text-white tracking-widest"
+                    >
+                        氏名
+                    </label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder=""
+                        className="bg-gray-300 border border-gray-400 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-1"
+                        // required
+                        {...register("name", {required: true})}
+                    />
+                    {errors.name && errorMessage}
+                </div>
+                <div>
+                    <label
+                        htmlFor="email"
+                        className="block mb-1 text-xs text-white tracking-widest"
+                    >
+                        Mail
+                    </label>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder=""
+                        className="bg-gray-300 border border-gray-400 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-1"
+                        // required
+                        {...register("email", {required: true})}
+                    />
+                    {errors.email && errorMessage}
+                </div>
+                <button
+                    type="submit"
+                    className="w-full cursor-pointer hover:opacity-80 px-2 pt-3 pb-5"
+                >
+                    <Image src={SendButton} alt="送信ボタン" className="object-contain"/>
+                </button>
+            </form>
+        </div>
+    )
 
 }
