245e3ec13c80766d979262d6db638a08f880fa1c
rmiyazaki-dramabase-inc <miyazaki@dramabase-inc.com>
2023-10-05 13:41:53 +0900

    feat(フォームデータ): フォーム送信されたデータをS3へ永続保存するよう修正
    - S3登録ロジックの追加
    - 登録に失敗した場合のエラーメッセージを表示するよう拡張

--- app/api/inquiry/route.js
+++ app/api/inquiry/route.js
@@ -1,7 +1,9 @@
 import {headers} from "next/headers";
+import FormRequestHandler from "@/app/api/FormRequestHandler";
 
 /*
  * TODO : Refactor to remove duplicated logic with `request/route.js`
+ * TODO : Better to implement CORS setup
  */
 export async function POST(request) {
     const body = await request.json();
@@ -9,10 +11,9 @@ export async function POST(request) {
     const headersList = headers()
     const host = headersList.get('Host');
     const userAgent = headersList.get('User-Agent');
-    console.info(`Inquiry Request : ${JSON.stringify(body)} by host: ${host}, user-agent: ${userAgent}`);
+    const reqInfo = `Inquiry Request : ${JSON.stringify(body)} by host: ${host}, user-agent: ${userAgent}`;
 
+    // Save request information into S3
+    return await FormRequestHandler('inquiry', reqInfo);
 
-    return new Response(JSON.stringify({message: "OK"}), {
-        status: 200,
-    })
 }
